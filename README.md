3393e03d3d9ec84e8135c2739b8f7b73b215005d
Agent플랫폼개발Cell 정찬명 <jeong.c.m@sk.com>
2022-07-24 00:15:18 +0900

    Let's try flex shorthand property!

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 * [display: flex \| inline-flex](flex-display.html)
 * [flex-flow: &lt;‘flex-direction’&gt; \|\| &lt;‘flex-wrap’&gt;](flex-flow.html)
-* [evn function](env-function.html)
-* [defer-css-optimized](defer-css-optimized.html)
-* [defer-css-unoptimized](defer-css-unoptimized.html)
+* [flex: none \| \[ \<‘flex-grow’\> \<‘flex-shrink’\>? \|\| \<‘flex-basis’\> \]](flex.html)
--- flex-example.js
+++ flex-example.js
@@ -1,24 +1,59 @@
-const baseStyleEl = document.querySelector('#base'); // style element
-const tryItEl = document.querySelector('#try');
-const demoTargetStr = document.querySelector('#applies').textContent; // 적용할 선택자
-const demoTargetEl = document.querySelector(demoTargetStr);
-const selectorEl = document.querySelector('#selector'); // 선택자
-const declarationEl = document.querySelector('#declaration'); // 선언부
-const codeEl = document.querySelector('#code'); // 코드
-const defaultValueStr = tryItEl.querySelector(':checked').nextElementSibling.textContent.replace(/\(.*\)/, ''); // 초깃값
+const baseStyleEl = document.querySelector('#base'); // style 요소
+const tryItEl = document.querySelector('#try'); // 라디오 버튼 컨테이너
+const demoTargetStr = document.querySelector('#applies').textContent; // 적용할 선택자 스트링
+const demoTargetEl = document.querySelectorAll(demoTargetStr); // 선택자 리스트
+const selectorEl = document.querySelector('#selector'); // 선택자 표시 요소
+const declarationEl = document.querySelector('#declaration'); // 선언부 표시 요소
+const baseOutputEl = document.querySelector('#base-output'); // 기본 코드 아웃풋
+const equivalentToEl = document.querySelector('#equivalent-to'); // 동등한 코드 아웃풋
+const equivalentArr = equivalentToEl && equivalentToEl.dataset.eq.split(' '); // 데이터셋에 있는 동등한 속성 배열
+const defaultValueStr = tryItEl.querySelector(':checked').nextElementSibling.textContent.replace(/\(.*\)/, ''); // 초깃값 문자열
+
+// 동등한 속성+값 문자열 구하기
+const getEquivalentStr = () => {
+    let computedStyleStr = '';
+    const computedStyleObj = window.getComputedStyle(demoTargetEl[0]);
+
+    for (let i = 0; i < equivalentArr.length; i++) {
+        computedStyleStr += equivalentArr[i] + ': ' + computedStyleObj.getPropertyValue(equivalentArr[i]) + ';\n';
+    }
+
+    return computedStyleStr;
+};
+
+// 예제에 스타일 적용하기
+const applyStyle = (valueStr) => {
+    for (let i = 0; i < demoTargetEl.length; i++) {
+        demoTargetEl[i].setAttribute('style', valueStr);
+    }
+};
+
+// 동등한 속성+값 표시하기
+const setEqStr = (valueStr) => {
+    if (!equivalentToEl) {
+        return;
+    }
+
+    equivalentToEl.textContent = valueStr + '\n==\n' + getEquivalentStr();
+};
 
 // 기본 스타일을 초깃값으로 적용하기
-selectorEl.textContent = demoTargetStr;
-declarationEl.textContent = defaultValueStr;
-demoTargetEl.setAttribute('style', defaultValueStr);
+selectorEl.textContent = demoTargetStr; // 선택자
+declarationEl.textContent = defaultValueStr; // 속성+값
+applyStyle(defaultValueStr); // 스타일 적용
+setEqStr(defaultValueStr); // 동등한 표현
 
 // 베이스 코드 표시하기
-codeEl.textContent = baseStyleEl.textContent;
+baseOutputEl.textContent = baseStyleEl.textContent;
 
 // 선택한 값을 데모에 적용하기
 tryItEl.addEventListener('change', (event) => {
-    const selectedValue = event.target.nextElementSibling.textContent.replace(/\(.*\)/, '');
+    const selectedValueStr = event.target.nextElementSibling.textContent.replace(/\(.*\)/, '');
 
-    declarationEl.textContent = selectedValue;
-    demoTargetEl.setAttribute('style', selectedValue);
+    declarationEl.textContent = selectedValueStr;
+    applyStyle(selectedValueStr);
+
+    setTimeout(() => {
+        setEqStr(selectedValueStr)
+    }, 400);
 });
